2523fa0331028986d1356a445e45c5ad45c796c4
piyush sen <piyushsen044@gmail.com>
2019-07-04 18:48:46 +0530

    update

---
+++ my-app/src/app/startingpage/startingpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-startingpage',
+  templateUrl: './startingpage.component.html',
+  styleUrls: ['./startingpage.component.scss']
+})
+export class StartingpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
