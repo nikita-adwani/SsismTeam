4c678eaf8ea2794203d6a4519d614f4bc7b23507
piyush sen <piyushsen044@gmail.com>
2019-07-05 12:42:36 +0530

    update

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -4,13 +4,11 @@ import { StudentListComponent } from './student-list/student-list.component';
 import { DetailComponent } from './detail/detail.component';
 import { DetailsComponent } from './details/details.component';
 import {  NgModule} from  '@angular/core';
- 
-
-
+import { StartingpageComponent } from './startingpage/startingpage.component';
 
 export const routes: Routes = [
 
-  { path: '', component: StudentListComponent },
+  { path: '', component: StartingpageComponent },
   { path: 'detail/:candidateId', component: DetailComponent },
   {path: "student-list", component:StudentListComponent},
   { path: 'details', component: DetailsComponent }
