befe3ec00fe910f8a400fabd02ecb9b6b69d68e6
pm9889 <51709274+pm9889@users.noreply.github.com>
2019-07-11 13:22:16 +0530

    confused

--- my-app/src/environments/environment.ts
+++ my-app/src/environments/environment.ts
@@ -5,19 +5,16 @@
 export const environment = {
   production: false,
   dataURL: "http://localhost:8080",
-<<<<<<< HEAD
-  rewardURL: "http://localhost:1010"
-=======
-  rewardURL: "http://localhost:9090"
+  rewardURL: "http://localhost:1010",
+  rewardURL: "http://localhost:9090",
   suhaseeRewardURL: "http://localhost:2121"
->>>>>>> 77ddfef4f11ca86389a101b67c9b046bd218d927
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// /*
+//  * For easier debugging in development mode, you can import the following file
+//  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+//  *
+//  * This import should be commented out in production mode because it will have a negative impact
+//  * on performance if an error is thrown.
+//  */
+// // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
